eec0154a7eac4996ee6ee4909110335090580cae
luoliyi <mopln510@qq.com>
2018-12-25 22:50:05 +0800

    第五次提交，模仿微信做了支付功能。

--- MUJIShow/src/main/webapp/js/myinfo.js
+++ MUJIShow/src/main/webapp/js/myinfo.js
@@ -7,7 +7,15 @@ $(function() {
 				time: 1500
 			});
 		});
-	})
+		/*
+		* 弹出详细界面
+		* */
+
+		$("#myorderInforation").css("display","block");
+		/*$(".listitem-wrap").css("display","none");
+        $(".has-title").css("display","none");*/
+
+	});
 	$("#lookallmyorders").click(function() {
 		$("#center_container iframe:eq(0)", parent.document).css("display", "block").attr("src", "myOrders.html?id=1");
 	});
